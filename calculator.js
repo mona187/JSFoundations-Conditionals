b422eb42b2cd7c6529a7394af9b5d7be07ff8923
Mona Abu Sharkh <mona@Monas-MacBook-Air.local>
2021-05-24 19:30:36 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
-const prompt = require("prompt-sync")(); // Please don't touch me :)
+var Firstnumber = 10;
+var secondnumber = 5;
+const operation = "/";
 
-// The prompt method allows us to take an input from the user.
-// Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+if (operation === "-") console.log(Firstnumber - secondnumber);
+else if (operation === "+") console.log(Firstnumber + secondnumber);
+else if (operation === "/") console.log(Firstnumber / secondnumber);
+else if (operation === "*") console.log(Firstnumber * secondnumber);
